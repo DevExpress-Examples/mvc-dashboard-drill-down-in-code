e5c11ec60c88d62308c0e8bd79609c2e0ccf5c26
DevExpressExampleBot <fakeEmail>
2021-10-11 22:36:14 +0300

    autogenerate VB

--- VB/MVCxDashboard_PerformDrillDown/App_Start/FilterConfig.vb
+++ VB/MVCxDashboard_PerformDrillDown/App_Start/FilterConfig.vb
@@ -1,8 +1,9 @@
-﻿Imports System.Web
 Imports System.Web.Mvc
 
 Namespace MVCxDashboard_PerformDrillDown
+
     Public Class FilterConfig
+
         Public Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
             filters.Add(New HandleErrorAttribute())
         End Sub
